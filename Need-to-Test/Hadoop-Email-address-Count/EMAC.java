39800141953fb98c9e59675b8da80c41b2fb91d4
山田かみら <superfishman9@gmail.com>
2014-06-22 16:55:59 +0800

    Create EMAC.java

---
+++ Need-to-Test/Hadoop-Email-address-Count/EMAC.java
@@ -0,0 +1,69 @@
+package org.myorg;
+
+import java.io.IOException;
+import java.util.*;
+ 
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.conf.*;
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapred.*;
+import org.apache.hadoop.util.*;
+
+public class EMAC {
+
+  public static class Map extends MapReduceBase implements 
+  Mapper<LongWritable, Text, Text, IntWritable> {
+
+    private final static IntWritable one = new IntWritable(1);
+    private Text word = new Text();
+
+    String emailreg = "^[_A-Za-z0-9-]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$";
+
+    public void map(LongWritable key, Text value, OutputCollector<Text, IntWritable> output, Reporter reporter) throws IOException {
+      
+
+      String line = value.toString();
+      StringTokenizer tokenizer = new StringTokenizer(line);
+      
+      while (tokenizer.hasMoreTokens()) {
+        
+        emailaddress = tokenizer.nextToken().toString();
+
+        if(emailaddress.matches(emailreg)){
+          String[] str = emailaddress.split("@");
+          output.collect(new Text(str[1]), one);
+        }
+      }
+    }
+  }
+
+  public static class Reduce extends MapReduceBase implements Reducer<Text, IntWritable, Text, IntWritable> {
+    public void reduce(Text key, Iterator<IntWritable> values, OutputCollector<Text, IntWritable> output, Reporter reporter) throws IOException {
+      int sum = 0;
+      while (values.hasNext()) {
+        sum += values.next().get();
+      }
+      output.collect( key , new IntWritable(sum));
+    }
+  }
+
+  public static void main(String[] args) throws Exception {
+    JobConf conf = new JobConf(EMAC.class);
+    conf.setJobName("EMail Address Count");
+
+    conf.setOutputKeyClass(Text.class);
+    conf.setOutputValueClass(IntWritable.class);
+
+    conf.setMapperClass(Map.class);
+    conf.setCombinerClass(Reduce.class);
+    conf.setReducerClass(Reduce.class);
+
+    conf.setInputFormat(TextInputFormat.class);
+    conf.setOutputFormat(TextOutputFormat.class);
+
+    FileInputFormat.setInputPaths(conf, new Path(args[0]));
+    FileOutputFormat.setOutputPath(conf, new Path(args[1]));
+
+    JobClient.runJob(conf);
+  }
+}
